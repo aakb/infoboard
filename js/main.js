aefe01c4a6ea398a7a153879460839536e9563c2
martinyde <yde001@gmail.com>
2014-08-12 10:44:45 +0200

    Martin Yde - Added slide for api changes

--- js/main.js
+++ js/main.js
@@ -14,6 +14,9 @@ var urls = {
   4: [
     {"url": "http://geckoboard.etek.dk/static_pages/nagios.html"}
   ],
+  4: [
+    {"url": "http://geckoboard.etek.dk/static_pages/api-changes.html"}
+  ],
   /*5: [
     {"url": "http://geckoboard.etek.dk/static_pages/burndown_charts.html"}
   ],*/
